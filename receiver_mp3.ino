d217c6866cb7bcb94e7d35170af01b4a6b54db1a
Yuri Sales <yurihenrique60@hotmail.com>
2015-10-07 21:04:07 -0300

    Update receiver_mp3.ino

--- receiver_mp3.ino
+++ receiver_mp3.ino
@@ -1,42 +1,37 @@
+//importa as bibliotecas necessárias para o funcionamento dos componentes
 #include <VirtualWire.h>
 #include <SPI.h>
 #include <SdFat.h>
 #include <SdFatUtil.h>
 #include <SFEMP3Shield.h>
 
-SdFat sd;
+SdFat sd; //variável para o cartão de memória
 SFEMP3Shield MP3player;
-const int led = 12;
-uint8_t resultado;
+const int led = 12; //variável para o led no pino 12 do arduino
 
 void setup() {
-  //Serial.begin(115200);
+  Serial.begin(115200); //inicializa a serial
+
+  //inicializa o receptor
   vw_set_ptt_inverted(true);
   vw_set_rx_pin(13);
   vw_setup(1000);
-  pinMode(led, OUTPUT);
   vw_rx_start();
 
-  if(!sd.begin(SD_SEL, SPI_HALF_SPEED)) sd.initErrorHalt();
-  if(!sd.chdir("/")) sd.errorHalt("sd.chdir");
-  
-  resultado = MP3player.begin();
-  MP3player.setBitRate(192);
-  MP3player.setVolume(10,10);
-
+  pinMode(led, OUTPUT); //inicializa o led
 }
 
 void loop() {
-  int8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
   uint8_t buflen = VW_MAX_MESSAGE_LEN;
 
-   if (vw_get_message(buf, &buflen)) {
-    if(buf[0]=='1') {
-      tocar();
+   if (vw_get_message(buf, &buflen)) { //se receber a mensagem
+    if(buf[0]=='1') { //se a mensagem for igual a 1 acende o led e liga o mp3
       digitalWrite(led, HIGH);
+      ativar_mp3();
     }
 
-    if(buf[0]=='0'){
+    if(buf[0]=='0'){ //se a mensagem for igual a 0 o led fica desligado
       digitalWrite(led,LOW);
     }
   }
@@ -44,6 +39,15 @@ void loop() {
   delay(100);
 }
 
-void tocar() {
-  MP3player.playTrack(1);
+//função para ativar o shield mp3
+void ativar_mp3() {
+  //inicializa o cartão de memória
+  if(!sd.begin(SD_SEL, SPI_HALF_SPEED)) sd.initErrorHalt();
+  if(!sd.chdir("/")) sd.errorHalt("sd.chdir");
+
+  MP3player.begin(); //inicializa o mp3 shield
+  MP3player.setBitRate(192);
+  MP3player.setVolume(10,10); //ajusta o volume
+  MP3player.playTrack(1); /*seleciona o toque mp3, que aqui, no caso, será 1 para
+  fins de teste*/
 }
