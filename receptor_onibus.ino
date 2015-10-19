f338a44be8e7eb85fa1e6f51345372c5cc009522
Yuri Sales <yurihenrique60@hotmail.com>
2015-10-18 21:51:28 -0300

    Update receptor_onibus.ino

--- receptor_onibus.ino
+++ receptor_onibus.ino
@@ -6,37 +6,30 @@ char recebido_RF_char[4];
 
 void setup() {
   Serial.begin(9600);
-  //Pino ligado ao pino DATA do receptor RF
-  vw_set_rx_pin(13);
-  //Velocidade de comunicacao (bits por segundo)
-  vw_setup(1000); 
-  //Inicia a recepcao  
-  vw_rx_start();
+  vw_set_rx_pin(13); //Pino ligado ao pino DATA do receptor RF
+  vw_setup(1000); //Velocidade de comunicacao (bits por segundo)
+  vw_rx_start();//Inicia a recepcao
   Serial.println("Recepcao modulo RF - Aguardando...");
 }
 
 void loop() {
-  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buf[VW_MAX_MESSAGE_LEN]; 
   uint8_t buflen = VW_MAX_MESSAGE_LEN;
     
   if (vw_get_message(buf, &buflen)) {
-    int i; 
+    int i; //contador 
     for (i = 0; i < buflen; i++) {            
-      //Armazena os caracteres recebidos  
-      recebido_RF_char[i] = char(buf[i]);
+      recebido_RF_char[i] = char(buf[i]); //Armazena os caracteres recebidos 
     }
     
     recebido_RF_char[buflen] = '\0';
-       
-    //Converte o valor recebido para integer
-    valor_recebido_RF = atoi(recebido_RF_char);
-         
-    //Mostra no serial monitor o valor recebido
+    valor_recebido_RF = atoi(recebido_RF_char); //Converte o valor recebido para integer
+    
     Serial.print("Recebido: ");
     Serial.print(valor_recebido_RF);
-    //Altera o estado do led conforme o numero recebido
+    
     if (valor_recebido_RF == 0) {
-      tone(buzzer, 1500);
+      tone(buzzer, 1500); //Liga o buzzer se o valor recebido for 0 (o valor que o transmissor do usuário enviará)
       
       delay(5000);
     }
