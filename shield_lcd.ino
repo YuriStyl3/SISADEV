e093bf1ac4e5cd1d5b11e6d8616c4a126ef96bbe
Yuri Sales <yuri.sales@protonmail.com>
2015-11-12 15:18:30 -0300

    Update shield_lcd.ino

--- shield_lcd.ino
+++ shield_lcd.ino
@@ -8,38 +8,39 @@ int botoes;
   
 void setup() {
   Serial.begin(9600);   
-  lcd.begin(16, 2);      
+  lcd.begin(16, 2); //Inicia o lcd shield
 }  
   
-void loop() {  
+void loop() {
+  lcd.setCursor(4,0);  
+  lcd.print("SISADEV");
+  
   botoes = analogRead (0); //Leitura do valor da porta analógica A0
-  atualiza_menu();    
+  atualiza_menu();  
 }
 
 void atualiza_menu() {
   lcd.setCursor(8,1);
 
-  if(botoes < 100 || botoes < 400) { //Se botão da esquerda for pressionado 
-      lcd.clear();
-      menu_cont--; //Volta para o MENU anterior
+  if(botoes < 100 || botoes < 200) { //se o botao direito ou o de cima forem clicados
+    lcd.clear(); //Limpa a tela
+    menu_cont++; //Vai para o proximo menu
     
-    } else if(botoes < 200 || botoes < 600) { //Senão se o botão da direita for pressionado
+  } else if(botoes < 400 || botoes < 600) { 
       lcd.clear();
-      menu_cont++;  //Vai para o próximo MENU
+      menu_cont--;  //Volta para o menu anterior
     
-    } else if(botoes < 800) {
+  } else if(botoes < 800) {
       sel = 1; //Valida que o botão de seleção foi pressionado
-    }
+  }
     
-    trata_botao(); //Chama a função para tratamento do botão para executar somente quando for clicado
-    menu_list();
+  trata_botao(); //Chama a função para tratamento do botão para executar somente quando for clicado
+  menu_list();
 }
 
 void menu_list() {
 
   if(menu_cont == 0) {
-    lcd.setCursor(4,0);  
-    lcd.print("SISADEV");
     lcd.setCursor(0,1); 
     lcd.print("04 - Amarante");
 
@@ -55,12 +56,10 @@ void menu_list() {
           sel = 0; //Sai do MENU
             trata_botao(); //Chama a função para tratamento do botão para executar somente quando for clicado
           }
-    	}*/
+        }*/
     }
   
   } else if(menu_cont == 1) {
-    lcd.setCursor(4,0);  
-  lcd.print("SISADEV");
     lcd.setCursor(0,1); 
     lcd.print("08 - Redinha");
     
@@ -76,12 +75,10 @@ void menu_list() {
           sel = 0; //Sai do MENU
             trata_botao(); //Chama a função para tratamento do botão para executar somente quando for clicado
           }
-    }*/
+        }*/
     }
   
   } else if(menu_cont == 2) {//Menu 3: Tela de Ajuste do Contraste
-    lcd.setCursor(4,0);  
-    lcd.print("SISADEV");
     lcd.setCursor(0,1); 
     lcd.print("64 - Nova Natal");
     
@@ -94,14 +91,12 @@ void menu_list() {
           trata_botao2(); //Chama a função para tratamento do botão para executar somente quando for clicado
           
           if(botoes < 100) {//Se o botão de seleção for pressionado
-            sel = 0; //Sai do MENU
+          sel = 0; //Sai do MENU
             trata_botao(); //Chama a função para tratamento do botão para executar somente quando for clicado
           }
         }*/
     }
   } else if(menu_cont == 3) {//Menu 4: Tela de Estado do Relé
-    lcd.setCursor(4,0);  
-  lcd.print("SISADEV");
     lcd.setCursor(0,1); 
     lcd.print("85 - Soledade");
     
@@ -117,7 +112,7 @@ void menu_list() {
           sel = 0; //Sai do MENU
             trata_botao(); //Chama a função para tratamento do botão para executar somente quando for clicado
           }
-      }*/
+        }*/
     }
   
   } else if(menu_cont == 4) {//Se a contagem de menu for para 4
