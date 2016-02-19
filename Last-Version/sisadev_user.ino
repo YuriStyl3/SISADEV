87e80339f519c3b61c00e2fabddc1fb58b2b0c0a
Yuri Sales <yuri.sales@protonmail.com>
2016-02-19 19:16:07 -0300

    Update sisadev_user.ino

--- Last-Version/sisadev_user.ino
+++ Last-Version/sisadev_user.ino
@@ -5,6 +5,7 @@
 #include <SFEMP3Shield.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <RH_NRF24.h>
 
 SdFat sd; //variável para o cartão de memória
 SFEMP3Shield MP3player;
@@ -12,9 +13,10 @@ SFEMP3Shield MP3player;
 String onibus;
 String linha_onibus[1];
 
-RF24 radio(9,10);
+RH_NRF24 nrf24;
+//RF24 radio(9,10);
 
-const uint64_t pipe = 0xE13CBAF433LL;
+//const uint64_t pipe = 0xE13CBAF433LL;
 const byte LINHAS = 4;
 const byte COLUNAS = 3;
 
@@ -33,9 +35,15 @@ Keypad teclado = Keypad(makeKeymap(matriz_teclas), PinosLinhas, PinosColunas, LI
 
 void setup() {
   Serial.begin(9600);
-  /*Serial.println("Teclado 4x3");
-  Serial.println("Aguardando o click do usuário...");
-  Serial.println();*/     
+  while (!Serial) 
+    ; // wait for serial port to connect. Needed for Leonardo only
+  if (!nrf24.init())
+    Serial.println("init failed");
+  // Defaults after init are 2.402 GHz (channel 2), 2Mbps, 0dBm
+  if (!nrf24.setChannel(1))
+    Serial.println("setChannel failed");
+  if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm))
+    Serial.println("setRF failed");     
 }
 
 void loop() {
@@ -44,7 +52,6 @@ void loop() {
   if (tecla_press) {
     if (tecla_press == '*') {
       int numero = (int)tecla_press;
-      //Serial.println(numero);
       onibus = "";
       //ativar_mp3(numero);
 
@@ -53,7 +60,6 @@ void loop() {
       Serial.println("Enviado");
       ativar_transceiver(onibus);
       onibus = "";
-      //Serial.println(numero);
       //ativar_mp3(numero);
       
     } else {
@@ -61,9 +67,9 @@ void loop() {
       Serial.print("Tecla pressionada: ");
       Serial.println(tecla_press);
       onibus += tecla_press;
-      linha_onibus[0] = onibus; 
+      Serial.print("Linha: ");
       Serial.println(onibus);
-      /*Serial.println(numero);*/
+      linha_onibus[0] = onibus; 
       //ativar_mp3(numero);
     }  
   } 
@@ -71,14 +77,37 @@ void loop() {
 }
 
 void ativar_transceiver(String onibus) {
-  int linha[1];
-  linha[0] = atoi(onibus.c_str());
-  radio.begin();
+  int n_onibus = atoi(onibus.c_str());
+  uint8_t linha[1];
+  linha[0] = n_onibus;  
+  Serial.println("Sending to nrf24_server");
+  nrf24.send(linha, sizeof(linha));
+  
+  nrf24.waitPacketSent();
+  
+  uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+  
+  if (nrf24.waitAvailableTimeout(500)) {
+    if (nrf24.recv(buf, &len)) {
+      Serial.print("Got reply: ");
+      Serial.println((char*)buf);
+    } else {
+        Serial.println("recv failed");
+    }
+  }else{
+      Serial.println("No reply, is nrf24_server running?");
+  }
+  
+  /*Serial.println(linha[0]);
+  Serial.println("--------------");*/
+  delay(400);
+  
+  /*radio.begin();
   radio.setChannel(100);
   radio.openWritingPipe(pipe);
-  radio.write(linha, 1);
-  Serial.println(linha[0]);
-  Serial.println("--------------");
+  radio.write(linha, 1);*/
+  
   
 }
 
