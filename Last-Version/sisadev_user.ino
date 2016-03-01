d80564a6fdf02711068e6115892d4608dda2b074
Yuri Sales <yuri.sales@protonmail.com>
2016-03-01 17:53:46 -0300

    Update sisadev_user.ino

--- Last-Version/sisadev_user.ino
+++ Last-Version/sisadev_user.ino
@@ -12,11 +12,18 @@ SFEMP3Shield MP3player;
 
 String onibus;
 String linha_onibus[1];
+float data[1];
 
-RH_NRF24 nrf24;
+
+#define CE_PIN 9
+#define CSN_PIN 10
+RF24 radio(CE_PIN, CSN_PIN);
+//RH_NRF24 nrf24;
 //RF24 radio(9,10);
 
-//const uint64_t pipe = 0xE13CBAF433LL;
+//byte canal[5] = {'s', 'i', 's', 'a', 'd'};
+
+const uint64_t pipe = 0xE8E8F0F0E1LL;
 const byte LINHAS = 4;
 const byte COLUNAS = 3;
 
@@ -34,8 +41,12 @@ byte PinosColunas[COLUNAS] = {4, 3, 2};
 Keypad teclado = Keypad(makeKeymap(matriz_teclas), PinosLinhas, PinosColunas, LINHAS, COLUNAS);
 
 void setup() {
+  radio.begin();
   Serial.begin(9600);
-  while (!Serial) 
+  radio.openWritingPipe(pipe);
+  /*radio.setPALevel(RF24_PA_HIGH);
+  radio.setDataRate(RF24_250KBPS);*/
+   /*while (!Serial) 
     ; // wait for serial port to connect. Needed for Leonardo only
   if (!nrf24.init())
     Serial.println("init failed");
@@ -43,7 +54,7 @@ void setup() {
   if (!nrf24.setChannel(1))
     Serial.println("setChannel failed");
   if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm))
-    Serial.println("setRF failed");     
+    Serial.println("setRF failed");*/     
 }
 
 void loop() {
@@ -77,13 +88,22 @@ void loop() {
 }
 
 void ativar_transceiver(String onibus) {
-  uint8_t bus = atoi(onibus.c_str()); 
+  data[0] = atof(onibus.c_str());
+  bool ok = radio.write(data, sizeof(data));
+  
+  if (ok) {
+    Serial.print("Dado enviado: ");
+    Serial.println(data[0]);
+  } else {
+    Serial.println("Nao foi possivel enviar...");
+  }
+  /*uint8_t bus = atoi(onibus.c_str()); 
   uint8_t data[1] = {bus};
   Serial.println("Sending to nrf24_server");
-  nrf24.send(data, sizeof(data));
+  nrf24.send(data, sizeof(data));*
   /*Serial.print("Enviado: ");
   Serial.println(data[0]);
-  Serial.println("---------------------------");*/
+  Serial.println("---------------------------");
   nrf24.waitPacketSent();
   
   uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
@@ -101,15 +121,14 @@ void ativar_transceiver(String onibus) {
   }
   
   /*Serial.println(linha[0]);
-  Serial.println("--------------");*/
-  delay(400);
+  Serial.println("--------------");
+  delay(400);*/
   
   /*radio.begin();
   radio.setChannel(100);
   radio.openWritingPipe(pipe);
   radio.write(linha, 1);*/
-  
-  
+  delay(1000);  
 }
 
 //função para ativar o shield mp3
