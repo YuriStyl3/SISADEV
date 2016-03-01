db2023dff4972adc5992b762ef230caff50c5bb2
Yuri Sales <yuri.sales@protonmail.com>
2016-03-01 17:53:29 -0300

    Update teste_receptor.ino

--- Last-Version/teste_receptor.ino
+++ Last-Version/teste_receptor.ino
@@ -1,16 +1,29 @@
 #include <SPI.h>
-#include <RH_NRF24.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define CE_PIN 9
+#define CSN_PIN 10
+ 
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+RF24 radio(CE_PIN, CSN_PIN);
+
+float data[1];
 
 // Singleton instance of the radio driver
-RH_NRF24 nrf24;
+//RH_NRF24 nrf24;
 // RH_NRF24 nrf24(8, 7); // use this to be electrically compatible with Mirf
 // RH_NRF24 nrf24(8, 10);// For Leonardo, need explicit SS pin
 // RH_NRF24 nrf24(8, 7); // For RFM73 on Anarduino Mini
 
-void setup() 
-{
+void setup() {
   Serial.begin(9600);
-  while (!Serial) 
+  radio.begin();
+  /*radio.setPALevel(RF24_PA_MAX);    
+  radio.setDataRate(RF24_250KBPS);*/  
+  radio.openReadingPipe(1, pipe);
+  radio.startListening();
+  /*while (!Serial) 
     ; // wait for serial port to connect. Needed for Leonardo only
   if (!nrf24.init())
     Serial.println("init failed");
@@ -18,12 +31,18 @@ void setup()
   if (!nrf24.setChannel(1))
     Serial.println("setChannel failed");
   if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm))
-    Serial.println("setRF failed");    
+    Serial.println("setRF failed");*/    
 }
 
-void loop()
-{
-  if (nrf24.available())
+void loop(){
+  if (radio.available()) {
+    radio.read(data, sizeof(data));
+    Serial.print("Recebido: ");
+    Serial.println(data[0]);    
+  } else {
+    Serial.println("Nao ha dados de radio disponiveis...");
+  }
+  /*if (nrf24.available())
   {
     // Should be a message for us now   
     uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
@@ -45,5 +64,6 @@ void loop()
     {
       Serial.println("recv failed");
     }
-  }
+  }*/
+  delay(1000);
 }
