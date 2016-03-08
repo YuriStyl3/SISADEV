f055ee456403e31219c44a86e6d4198abd5313d5
Yuri Sales <yuri.sales@protonmail.com>
2016-03-08 19:12:00 -0300

    Update BUS.ino

--- First-Version/BUS.ino
+++ First-Version/BUS.ino
@@ -10,30 +10,29 @@ int sel=0; //Variável para verificar estado do botão selecionar
 int botoes;
 int valor_recebido_RF;
 char recebido_RF_char[4];
+int onibus;
   
 void setup() {
   Serial.begin(9600);   
-  
   lcd.begin(16, 2); //Inicia o lcd shield
   digitalWrite(Backlight, HIGH);
-  
-  vw_set_rx_pin(50); //Pino ligado ao pino DATA do receptor RF
-  vw_setup(1000); //Velocidade de comunicacao (bits por segundo)
-  vw_rx_start();//Inicia a recepcao
-  Serial.println("Recepcao modulo RF - Aguardando...");
 }  
   
 void loop() {
   verificar_receptor();
   lcd.setCursor(4,0);  
   lcd.print("SISADEV");
-  
   botoes = analogRead (0); //Leitura do valor da porta analógica A0
   atualiza_menu();
-  
+  verificar_receptor();
 }
 
 void verificar_receptor() {
+  vw_set_rx_pin(50); //Pino ligado ao pino DATA do receptor RF
+  vw_setup(1000); //Velocidade de comunicacao (bits por segundo)
+  vw_rx_start();//Inicia a recepcao
+  Serial.println("Recepcao modulo RF - Aguardando...");
+  
   uint8_t buf[VW_MAX_MESSAGE_LEN]; 
   uint8_t buflen = VW_MAX_MESSAGE_LEN;
     
@@ -49,12 +48,14 @@ void verificar_receptor() {
     Serial.print("Recebido: ");
     Serial.println(valor_recebido_RF);
     
+    ativar_transmissor(valor_recebido_RF);
     /*if (valor_recebido_RF == 0) {
     //tone(buzzer, 1500); //Liga o buzzer se o valor recebido for 0 (o valor que o transmissor do usuário enviará)
     Serial.println("Valor 0 recebido");  
     }*/  
     delay(5000);  
   }
+  delay(1000);
 }
 
 void atualiza_menu() {
@@ -81,7 +82,7 @@ void menu_list() {
     lcd.print("04 - Amarante");
 
     if(sel == 1) {//Se botão de seleção for pressionado
-    
+      onibus = 4;
       /*while(sel == 1) {//Enquanto o botão de selecionar não for pressionado denovo
       
           botoes = analogRead(A0); //Lê os botões
@@ -100,7 +101,7 @@ void menu_list() {
     lcd.print("08 - Redinha");
     
     if(sel == 1) {//Se botão de seleção for pressionado
-    
+      onibus = 8;    
       /*while(sel == 1) {//Enquanto o botão de selecionar não for pressionado denovo
       
           botoes = analogRead(A0); //Lê os botões
@@ -119,7 +120,7 @@ void menu_list() {
     lcd.print("64 - Nova Natal");
     
     if(sel == 1) {//Se botão de seleção for pressionado
-    
+      onibus = 64;
       /*while(sel == 1) {//Enquanto o botão de selecionar não for pressionado denovo
       
           botoes = analogRead(A0); //Lê os botões
@@ -137,7 +138,7 @@ void menu_list() {
     lcd.print("85 - Soledade");
     
     if(sel == 1) {//Se botão de seleção for pressionado
-    
+      onibus = 85;
       /*while(sel == 1) {//Enquanto o botão de selecionar não for pressionado denovo
       
           botoes = analogRead(A0); //Lê os botões
@@ -157,6 +158,25 @@ void menu_list() {
   } else if(menu_cont == -1) {//Se a contagem de menu for para -1
     menu_cont = 3; //Volta para o último menu
   }
+}
+
+void ativar_transmissor(int valor_recebido) {
+  if (valor_recebido == onibus) {
+    vw_rx_stop(); //O receptor para de receber dados
+    
+    char *confirmacao = "1";
+    
+    vw_set_tx_pin(52); //Define o pino do transmissor
+    vw_setup(1000);
+ 
+    for (int i = 0; i < 10; i++){
+      vw_send((uint8_t *)confirmacao, strlen(confirmacao));
+      vw_wait_tx();
+      Serial.print("Valor enviado: ");
+      Serial.println(confirmacao);
+      delay(1000);
+    }
+  } 
 }
 
 void trata_botao() {
